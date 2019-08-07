51ca04ea8b7e10fddb2b5c386b6fa0e7ca7de4fa
Sir-Williams <sirjohnwilliams@live.com>
2019-08-07 16:37:37 -0700

    id was working, just using it wrong in the url

--- index.js
+++ index.js
@@ -34,18 +34,35 @@ server.get('/api/users', (req, res) => {
 });
 
 server.get('/api/users/:id', (req, res) => {
-    const userID = req.params.id;
-    if (!userID) {
-        res.status(404).json({ error: 'No user was found with this id'})
+    const userId = req.params.id;
+
+    if (!userId) {
+        res.status(404).json({ message: 'No user was found with this id'})
     }
-    db.findById()
-        .then(users => {
-            res.status(200).json(users);
+    db.findById(userId)
+        .then(userId => {
+            res.status(200).json(userId);
         })
         .catch(err => {
             res.status(500).json({ error: 'Information about this user is not avalible'})
         });
 });
+
+server.delete('api/user/:id', (req, res) => {
+    const userId = req.params.id;
+
+    db.remove(userId)
+        .then(user => {
+            if(!user) {
+                res.status(404).json({ message: 'No user was found with this id' })
+            } else {
+                res.status(201).json({ message: 'User was Deleted'})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'This user could not be deleted' });
+        })
+})
 
 
 
