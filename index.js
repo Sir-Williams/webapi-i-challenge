9e434ca513f20d0a2a1b79d3c352324b63f14a0d
Sir-Williams <sirjohnwilliams@live.com>
2019-08-07 15:07:10 -0700

    can make a git and post request

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// implement your API here
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    console.log('inside get request')
+    res.send('<h1>Hi Lambda!</h1k>')
+});
+
+server.get('/users', (req, res) => {
+    const users = [
+        {
+            name: 'Sir',
+            bio: 'I like Naruto',
+            created_at: Date(),
+            updated_at: Date(),
+        },
+        {
+            name: 'Marc',
+            bio: 'I am very good at playing csgo',
+            created_at: Date(),
+            updated_at: Date(),
+        }
+    ];
+
+    res.status(200).json(users);
+})
+
+server.post('/users', (req, res) => {
+    res.status(201).json({ url: '/users', operation: 'POST' })
+})
+
+server.listen(9090, () => {
+    console.log('Listening to port 9090');
+});
