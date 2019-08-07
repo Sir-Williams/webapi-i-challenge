a0d8d1ff8cbfa0a5b10bdc86b25bd70d628eb009
Sir-Williams <sirjohnwilliams@live.com>
2019-08-07 15:42:22 -0700

    added get request

--- index.js
+++ index.js
@@ -4,22 +4,36 @@ const db = require('./data/db.js');
 
 const server = express();
 
+server.use(express.json());
+
 server.post('/api/users', (req, res) => {
     const newPerson= req.body;
-    const { name, bio } =req.body;
+    const { name, bio } = req.body;
 
     if (!name || !bio) {
         res.status(400).json({error: 'You are required to insert "name" and "bio".'})
     } else {
-        db.insert(newPerson)
-            .then(addedPerson => {
-                res.status(201).json(addedPerson)
+        Users.insert(newPerson)
+            .then(user => {
+                res.status(201).json({ user })
             })
             .catch(err => { 
                 res.status(500).json({ error: 'Could not save this person to the list'})
             })
     }
 })
+
+server.get('/api/users', (req, res) => {
+    Users.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'Information about this user is not avalible'})
+        });
+});
+
+
 
 
 
